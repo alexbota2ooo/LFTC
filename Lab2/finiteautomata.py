227adaada412552fd0e8d5d23433f9d5de0abb18
alexbota2ooo <alexbota60@yahoo.com>
2021-11-12 18:25:03 +0200

    presented lab2

--- Lab2/finiteautomata.py
+++ Lab2/finiteautomata.py
@@ -1,10 +1,10 @@
 class FiniteAutomata:
     def __init__(self, file):
-        self.__Q = []  #states
-        self.__S = {}  #transitions
-        self.__E = []   #alphabet
-        self.__F = []  #final states
-        self._q0 = "" #initial state
+        self.__Q = []
+        self.__S = {}
+        self.__E = []
+        self.__F = []
+        self._q0 = ""
         self.__file = file
         self.readFile()
 
@@ -34,3 +34,42 @@ class FiniteAutomata:
                     self.__S[(state1, route)].append(state2)
                 else:
                     self.__S[(state1, route)] = [state2]
+            for elem in self.__S.keys():
+                if len(self.__S[elem]) == 2:
+                    if self.__S[elem][0] == self.__S[elem][1]:
+                        self.__S[elem] = [self.__S[elem][0]]
+
+    def getStates(self):
+        return self.__Q
+
+    def getAlphabet(self):
+        return self.__E
+
+    def getTransitions(self):
+        return self.__S
+
+    def getInitalState(self):
+        return self.__q0
+
+    def getFinalStates(self):
+        return self.__F
+
+    def is_DFA(self):
+        for elem in self.__S.keys():
+            if len(self.__S[elem]) > 1:
+                return False
+        return True
+
+    def is_seq_accepted(self, sequence):
+        if self.is_DFA():
+            state = self.__q0
+            for symbol in sequence:
+                if (state, symbol) in self.__S.keys():
+                    state = self.__S[(state, symbol)][0]
+                else:
+                    return False
+            if state in self.__F:
+                return True
+            return False
+        return False
+
--- Lab2/main.py
+++ Lab2/main.py
@@ -13,5 +13,38 @@ def print_hi(name):
 if __name__ == '__main__':
     fa = FiniteAutomata("fa.in")
     fa.readFile()
+    print(fa.getStates())
+    print("Initial state: " + fa.getInitalState())
+    print(fa.getAlphabet())
+    print(fa.getTransitions())
+    while(True):
+        print("1. Initial state")
+        print("2. Final states")
+        print("3. All states")
+        print("4. Alphabet")
+        print("5. Transitions")
+        print("6. Is DFA")                 #is deterministic?
+        print("7. Is sequence accepted by the FA")
+        print("8. Exit")
+        command = input("Command : ")
+        if command == '1':
+            print(fa.getInitalState())
+        elif command == '2':
+            print(fa.getFinalStates())
+        elif command == '3':
+            print(fa.getStates())
+        elif command == '4':
+            print(fa.getAlphabet())
+        elif command == '5':
+            print(fa.getTransitions())
+        elif command == '6':
+            print(fa.is_DFA())
+        elif command == '7':
+            sequence = input("Sequence: ")
+            sequence = sequence.split()
+            #print(sequence)
+            print(fa.is_seq_accepted(sequence))
+        elif command == '8':
+            break
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
