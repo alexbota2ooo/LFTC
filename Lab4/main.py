f90ae5f048439185f42a4fd4e55eb73a12079754
alexbota2ooo <alexbota60@yahoo.com>
2021-11-19 18:01:49 +0200

    grammar alex&stefan

---
+++ Lab4/main.py
@@ -0,0 +1,26 @@
+from grammar import *
+
+if __name__ == '__main__':
+    option=-1
+    grammar = Grammar("g1.in")
+    print(grammar.checkCFG())
+    print("0. Exit")
+    print("1. Set of NonTerminals")
+    print("2. The alphabet")
+    print("3. Starting Symbol")
+    print("4. Productions")
+    print("5. Get productions for a given non-terminal")
+
+    while(option!=0):
+        option=int(input("option:"))
+        if option==1:
+            print(grammar.getNonTerms())
+        elif option==2:
+            print(grammar.getAlphabet())
+        elif option==3:
+            print(grammar.getStartingSymb())
+        elif option==4:
+            print(grammar.getProductions())
+        elif option==5:
+            nonterm = input("non-terminal")
+            print(grammar.getProdForNonTerm(nonterm)) 
